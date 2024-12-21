e61c79f6199f00b1bf92535d041d4981ab0e24c4
BelanVaidehee <133239808+BelanVaidehee@users.noreply.github.com>
2024-12-21 18:10:30 +0530

    Update Table.java

--- src/Table.java
+++ src/Table.java
@@ -5,6 +5,7 @@ public class Table {
     public static void main(String[] args) {
         System.out.print("\033[H\033[2J");
         System.out.flush();
+        //edited by Ishani
         
         Scanner scanner = new Scanner(System.in);
 
